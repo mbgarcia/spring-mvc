2d61f0437effd1a59cd94e75d14bf4e6fe2edcc3
Marcelo Garcia <mbgarcia.pa@gmail.com>
2019-06-05 15:52:56 -0300

    Updating user

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,7 +29,7 @@ public class UserController {
 	}
      
     @GetMapping("/new")
-    public String showFormCreate(User user) {
+    public String showCreateForm(User user) {
         return "/users/new";
     }
      
@@ -42,4 +43,13 @@ public class UserController {
         
         return "redirect:/users";
     }
+    
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+         
+        model.addAttribute("user", user);
+        return "/users/edit";
+    }    
 }
