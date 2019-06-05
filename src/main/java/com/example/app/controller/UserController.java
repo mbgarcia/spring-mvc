715974f40fc6c6a82c0dcfa8f3786b63aad363ef
Marcelo Garcia <mbgarcia.pa@gmail.com>
2019-06-05 15:42:59 -0300

    add new user

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.app.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.app.model.User;
 import com.example.app.repository.UserRepository;
 
 @Controller
@@ -21,4 +26,20 @@ public class UserController {
 		
 		return "/users/index";
 	}
+     
+    @GetMapping("/new")
+    public String showFormCreate(User user) {
+        return "/users/new";
+    }
+     
+    @PostMapping("/save")
+    public String save(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "/users/new";
+        }
+         
+        userRepository.save(user);
+        
+        return "redirect:/users";
+    }
 }
